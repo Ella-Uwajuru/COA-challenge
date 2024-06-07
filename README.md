e276d3e45056e02d635e0ecf00510b76350271e6
Ella-Uwajuru <ellapacissingizwa@gmail.com>
2024-06-07 21:58:04 +0200

    applying the grayscale

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COA-challenge
 Repository for Code of Africa (COA) challenge solutions. Explore solutions to various coding challenges provided by Code of Africa, showcasing problem-solving skills and algorithmic thinking. Each challenge solution is carefully documented and implemented in a clear and concise manner.
+
+
--- photo-gallery/script.js
+++ photo-gallery/script.js
@@ -3,17 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
     galleryItems.forEach(item => {
         item.addEventListener('mouseover', () => {
-            // Apply color and blur adjustments on mouseover
-            updateColorEffect(item, 0, 1, 0.8, '5px');
+            updateColorEffect(item, 0, 1.5, 0.4, '5px');
         });
 
         item.addEventListener('mouseout', () => {
-            // Reset color and blur adjustments on mouseout
             updateColorEffect(item, 0, 1, 1, '0px');
         });
     });
 
-    // Function to update the color and blur effect
     function updateColorEffect(target, hueRotation, saturationFactor, brightnessFactor, blurAmount) {
         target.style.filter = `
             hue-rotate(${hueRotation}deg)
